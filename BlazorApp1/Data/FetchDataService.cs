3c2aab3aa1061a584ab6a26b3afd1d646a3d389b
Arvin Roman Agramon <aragramon@gmail.com>
2025-08-17 00:34:38 +0800

    Add project files.

---
+++ BlazorApp1/Data/FetchDataService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp1.Data
+{
+    public class FetchDataService
+    {
+        
+
+        public async static Task<List<JsonData>> FetchData()
+        {
+            HttpClient httpClient = new HttpClient();
+            var result = await httpClient.GetFromJsonAsync<List<JsonData>>("https://jsonplaceholder.typicode.com/posts");
+
+            return result;
+        }
+        
+    }
+
+    public class JsonData
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+    }
+}
